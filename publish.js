f409a54620769957d9f15c80e8199e6993c44a72
Nico Lindemann <n.lindemann@globit.com>
2018-04-03 13:58:33 +0200

    fix: check for node

--- publish.js
+++ publish.js
@@ -336,7 +336,7 @@ function buildSubNavMembers(list, type) {
         html += '<ul class="inner">'
         list.forEach(function(item) {
             html += `<li class="sub-nav-item">${linkto(item.longname, item.name, null, null, true)}`
-            if (item.meta.code.node.async) {
+            if (item.meta.code.node && item.meta.code.node.async) {
                 html += ' <span class="async"> async</span>'
             }
             html += '</a></li>'
